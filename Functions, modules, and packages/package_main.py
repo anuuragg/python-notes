e28ac0643bda5f32539856de13793f4d6576d52e
Anurag <anuragcpyadav031@gmail.com>
2024-04-21 13:29:47 +0530

    packages.py

---
+++ Functions, modules, and packages/package_main.py
@@ -0,0 +1,6 @@
+from mypackage.arithmetic import add, subtract, multiply, divide
+
+print("Addition:", add(5, 3))      
+print("Subtraction:", subtract(5, 3))  
+print("Multiplication:", multiply(5, 3)) 
+print("Division:", divide(6, 3))   
