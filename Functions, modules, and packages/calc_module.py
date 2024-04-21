ab66e30e1f441968dd929b238fd3c13a54573c92
Anurag <anuragcpyadav031@gmail.com>
2024-04-21 13:13:20 +0530

    modules.py

---
+++ Functions, modules, and packages/calc_module.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, num1, num2):
+        print(num1,"+", num2, "=", num1+num2)
+        
+    def subtract(self, num1, num2):
+        print(num1,"-", num2, "=", num1-num2)
+        
+    def multiply(self, num1, num2):
+        print(num1,"*", num2, "=", num1*num2)
+        
+    def divide(self, num1, num2):
+        print(num1,"/", num2, "=", num1/num2)
---
+++ Functions, modules, and packages/module_call.py
@@ -0,0 +1,8 @@
+import calc_module
+
+calculator = calc_module.Calculator()
+
+calculator.add(10, 5)
+calculator.subtract(10, 5)
+calculator.multiply(10, 5)
+calculator.divide(10, 5) 
